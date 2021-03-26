1ccfce8966fdabed0e818343b28da8c72f923b6d
Mukka Deepak <ramu92013@gmail.com>
2021-03-26 23:43:36 +0530

    markdown done

--- Frontend/skionetizy/src/Components/markDown.js
+++ Frontend/skionetizy/src/Components/markDown.js
@@ -1,18 +1,38 @@
-import React from 'react'
-import './marDown.css'
+import React, { useState } from "react";
+import "./marDown.css";
+import ReactMarkdown from "react-markdown";
 function MarkDown() {
-    return (
-        <div className="mainmark">
-            <div style={{position:'absolute',left:'5%',top:'14%'}}>
-            <p>Enter document name</p>
-            <br/>
-            <input style={{backgroundColor:'#3498db',color:'white',padding:'5px',border:'1px solid transparent',outline:'none',borderRadius:'5px',width:'110%'}}/>
-
-            </div>
-           <textarea  rows="40" cols="85" className="textarea"/>
-           <textarea  rows="40" cols="80" className="markdown"/>
-        </div>
-    )
+  const [input, setInput] = useState("");
+  return (
+    <div className="mainmark">
+      <div style={{ position: "absolute", left: "5%", top: "14%" }}>
+        <p>Enter document name</p>
+        <br />
+        <input
+          style={{
+            backgroundColor: "#3498db",
+            color: "white",
+            padding: "5px",
+            border: "1px solid transparent",
+            outline: "none",
+            borderRadius: "5px",
+            width: "110%",
+          }}
+        />
+      </div>
+      <textarea
+        autoFocus
+        rows="40"
+        cols="85"
+        className="textarea"
+        fixed
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+      <ReactMarkdown source={input} className="markdown" />
+    </div>
+  );
 }
 
 export default MarkDown;
