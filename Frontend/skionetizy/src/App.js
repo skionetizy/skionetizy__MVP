012ea82c882a2e6901f80c739ec7fb5a8217d0a0
sam <samcse95@gmail.com>
2021-05-12 14:11:14 +0530

    MyBloge page is ready

--- Frontend/skionetizy/src/App.js
+++ Frontend/skionetizy/src/App.js
@@ -5,7 +5,7 @@ import MarkDown from './Components/markDown'
 import { BrowserRouter as Router , Link, Switch, Route} from 'react-router-dom'
 import Upload from './Components/uploadImage'
 import { FinalPage } from './Components/finalPage'
-
+import MyBlogs from './Components/MyBlogs'
 function App() {
   return (
      <Router>
@@ -17,7 +17,8 @@ function App() {
     <Route exact path="/mark" component={MarkDown}/>
     <Route exact path="/upload" component={Upload}/>
     <Route exact path="/final" component={FinalPage}/>
-      
+    <Route exact path="/myblogs" component={MyBlogs}/>
+    
       </Switch>
     </div>
     </Router>
---
+++ Frontend/skionetizy/src/Components/MyBlogs.js
@@ -0,0 +1,154 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import "./MyBlogs.css"
+function MyBlogs() {
+    return (
+
+        <div className="container">
+            {/* ---------------------card section starts here------------------- */}
+            {/* ----------------1st card-------------- */}
+            <div className="card">
+            <div className="card-image"></div>
+            <div className="card-text">
+                <div className="date">4 days ago</div>
+                    <h2>post 1</h2>
+                    <p>     Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit.Saepe eos id quisquam nemo magni, dolores fugit veritatis,maxime,
+                    aliquam debitis quod consectetur totam nihil voluptatem corrupti nisi et
+                    recusandae officia?
+                    </p>
+            </div>  
+                <div className="card-stats">
+                    <div className="stat">
+                        <div className="value">123</div>
+                        <div className="type">read</div>
+                    </div>
+                    <div className="stat">
+                        <div className="value">125</div>
+                        <div className="type">views</div>
+                    </div>
+                    <div className="stat">
+                        <div className="value">21</div>
+                        <div className="type">likes</div>
+                    </div>
+                </div> 
+                
+            </div>
+            {/* ----------------1st card-------------- */}
+            <div className="card">
+            <div className="card-image"></div>
+            <div className="card-text">
+                <div className="date">4 days ago</div>
+                    <h2>post 1</h2>
+                    <p>     Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit.Saepe eos id quisquam nemo magni, dolores fugit veritatis,maxime,
+                    aliquam debitis quod consectetur totam nihil voluptatem corrupti nisi et
+                    recusandae officia?
+                    </p>
+            </div>  
+                <div className="card-stats">
+                    <div className="stat">
+                        <div className="value">123</div>
+                        <div className="type">read</div>
+                    </div>
+                    <div className="stat">
+                        <div className="value">125</div>
+                        <div className="type">views</div>
+                    </div>
+                    <div className="stat">
+                        <div className="value">21</div>
+                        <div className="type">likes</div>
+                    </div>
+                </div> 
+                
+            </div>
+            {/* ----------------1st card-------------- */}
+            <div className="card">
+            <div className="card-image"></div>
+            <div className="card-text">
+                <div className="date">4 days ago</div>
+                    <h2>post 1</h2>
+                    <p>     Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit.Saepe eos id quisquam nemo magni, dolores fugit veritatis,maxime,
+                    aliquam debitis quod consectetur totam nihil voluptatem corrupti nisi et
+                    recusandae officia?
+                    </p>
+            </div>  
+                <div className="card-stats">
+                    <div className="stat">
+                        <div className="value">123</div>
+                        <div className="type">read</div>
+                    </div>
+                    <div className="stat">
+                        <div className="value">125</div>
+                        <div className="type">views</div>
+                    </div>
+                    <div className="stat">
+                        <div className="value">21</div>
+                        <div className="type">likes</div>
+                    </div>
+                </div> 
+                
+            </div>
+            {/* ----------------1st card-------------- */}
+            <div className="card">
+            <div className="card-image"></div>
+            <div className="card-text">
+                <div className="date">4 days ago</div>
+                    <h2>post 1</h2>
+                    <p>     Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit.Saepe eos id quisquam nemo magni, dolores fugit veritatis,maxime,
+                    aliquam debitis quod consectetur totam nihil voluptatem corrupti nisi et
+                    recusandae officia?
+                    </p>
+            </div>  
+                <div className="card-stats">
+                    <div className="stat">
+                        <div className="value">123</div>
+                        <div className="type">read</div>
+                    </div>
+                    <div className="stat">
+                        <div className="value">125</div>
+                        <div className="type">views</div>
+                    </div>
+                    <div className="stat">
+                        <div className="value">21</div>
+                        <div className="type">likes</div>
+                    </div>
+                </div> 
+                
+            </div>
+            {/* ----------------1st card-------------- */}
+            <div className="card">
+            <div className="card-image"></div>
+            <div className="card-text">
+                <div className="date">4 days ago</div>
+                    <h2>post 1</h2>
+                    <p>     Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit.Saepe eos id quisquam nemo magni, dolores fugit veritatis,maxime,
+                    aliquam debitis quod consectetur totam nihil voluptatem corrupti nisi et
+                    recusandae officia?
+                    </p>
+            </div>  
+                <div className="card-stats">
+                    <div className="stat">
+                        <div className="value">123</div>
+                        <div className="type">read</div>
+                    </div>
+                    <div className="stat">
+                        <div className="value">125</div>
+                        <div className="type">views</div>
+                    </div>
+                    <div className="stat">
+                        <div className="value">21</div>
+                        <div className="type">likes</div>
+                    </div>
+                </div> 
+                
+            </div>
+        </div>
+
+    )
+}
+
+export default MyBlogs
