cb35f145d5736ad5eef42ef31f6e4d543c10125e
KarthikRaju391 <karthikraju391@gmail.com>
2021-07-12 19:49:19 +0530

    changed the character count position

--- Frontend/skionetizy/src/Components/markDown.js
+++ Frontend/skionetizy/src/Components/markDown.js
@@ -136,7 +136,7 @@ function MarkDown(props) {
             onChange={handleChange("blogTitle")}
           />
         </div>
-        <div>characters Remaining {charactersRemaining}</div>
+        <div className="characters">characters Remaining {charactersRemaining}</div>
         <textarea
           autoFocus
           rows="40"
