090161db7a6f621ca49decd272f10700bdd2db93
NarayanAdithya <narayanadithya1234@gmail.com>
2021-07-24 20:45:54 +0530

    Added APP

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from backend import app,api
 from backend import initialize_db,db
 from backend.resources.routes import initialize_routes
+from flask import send_from_directory
 
 if __name__=="__main__":
     initialize_db(app)
     initialize_routes(api)
-    app.run()
+    @app.route('/')
+    @app.route('/home',methods=['GET'])
+    def home():
+        return send_from_directory(app.static_folder,'index.html')
+
+    @app.errorhandler(404)
+    def not_found(e):
+        return app.send_static_file('index.html')
+        app.run()
--- backend/__init__.py
+++ backend/__init__.py
@@ -44,11 +44,3 @@ db = MongoEngine()
 def initialize_db(app):    
     db.init_app(app)
 
-@app.route('/')
-@app.route('/home',methods=['GET'])
-def home():
-    return send_from_directory(app.static_folder,'index.html')
-
-@app.errorhandler(404)
-def not_found(e):
-    return app.send_static_file('index.html')
