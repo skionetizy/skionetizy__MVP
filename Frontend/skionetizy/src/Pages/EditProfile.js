d35a58fbf64de8a75d3d1dae7b83d58ca4fe45fb
jagdishlove <jagdish.funn@gmail.com>
2021-07-30 15:54:32 +0530

    Temp_style_added_editProfile

--- Frontend/skionetizy/src/Pages/EditProfile.js
+++ Frontend/skionetizy/src/Pages/EditProfile.js
@@ -1,62 +1,87 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import style from '../Pages/editProfile.module.css'
 import { useParams } from "react-router-dom";
 import {
-  getProfileDetailsAPIHandler,
-  updateProfileDetails,
+    getProfileDetailsAPIHandler,
+    updateProfileDetails,
 } from "../API/profileAPIHandler";
 import FormInput from "../Components/FormInput";
 import useForm from "../hooks/useForm";
 
 const initailData = {
-  profileBio: "",
-  profileWebsiteURL: "",
-  profilePicImage: null,
-  rofileBannerImage: null,
+    profileBio: "",
+    profileWebsiteURL: "",
+    profilePicImage: null,
+    rofileBannerImage: null,
 };
 
 export default function EditProfile() {
-  const { data, getInputProps, handleChange, setData } = useForm(initailData);
-  const [status, setStatus] = useState("idle");
-  const { profileUserName } = useParams();
+    const { data, getInputProps, handleChange, setData } = useForm(initailData);
+    const [status, setStatus] = useState("idle");
+    const { profileUserName } = useParams();
 
-  function handleSubmit(ev) {
-    ev.preventDefault();
-    setStatus("loading");
-    updateProfileDetails(data.profileID, data);
-    setStatus("success");
-  }
+    function handleSubmit(ev) {
+        ev.preventDefault();
+        setStatus("loading");
+        updateProfileDetails(data.profileID, data);
+        setStatus("success");
+    }
 
-  useEffect(() => {
-    getProfileDetailsAPIHandler(profileUserName).then((response) => {
-      setData(response.profile);
-    });
-  }, [profileUserName, setData]);
+    useEffect(() => {
+        getProfileDetailsAPIHandler(profileUserName).then((response) => {
+            setData(response.profile);
+        });
+    }, [profileUserName, setData]);
 
-  return (
-    // remove this style tag
-    <div style={{ color: "white" }}>
-      <form onSubmit={handleSubmit}>
-        {/* Banner image */}
-        {/* onChange & name is coming from getInputProps */}
-        <p>Banner</p>
-        <input type="file" name="profilePicImage" onChange={handleChange} />
-        {/* Profile image */}
-        <p>Profile</p>
-        <input type="file" name="profilePicImage" onChange={handleChange} />
+    return (
 
-        <FormInput className="" label="Bio" {...getInputProps("profileBio")} />
 
-        <FormInput
-          className=""
-          label="Website URL"
-          {...getInputProps("profileWebsiteURL")}
-        />
 
-        <button disabled={status === "loading"} type="submit">
-          Save
-        </button>
-      </form>
-    </div>
-  );
+        <form className={style.edit_form} onSubmit={handleSubmit}>
+            {/* Banner image */}
+            {/* onChange & name is coming from getInputProps */}
+
+            <div className={style.banner}>
+                <label>
+
+                    <input type="file" name="profilePicImage" onChange={handleChange} />
+                    <img
+                        className={style.coverImage}
+                        src="//unsplash.it/700/700"
+                        alt=""
+                    />
+
+                </label>
+
+            </div>
+            {/* Profile image */}
+            <div class={style.profilePic}>
+                <label>
+                    
+
+                    <img
+                        src="//unsplash.it/120/120"
+                        alt=""
+                    />
+                    <input type="file" name="profilePicImage" onChange={handleChange} />
+                </label>
+                </div>
+            <div className={style.bio}>
+                <label>Bio</label>
+                <textarea className=""  {...getInputProps("profileBio")} />
+            </div>
+            <FormInput
+                className=""
+                label="Website URL"
+                {...getInputProps("profileWebsiteURL")}
+            />
+
+            <button className={style.saveButton} disabled={status === "loading"} type="submit">
+                Save
+            </button>
+        </form>
+
+
+    );
 }
