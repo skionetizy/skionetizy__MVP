ffd7bb545be332f93fee1be5e40aba0343b38cc7
NarayanAdithya <narayanadithya1234@gmail.com>
2021-09-04 17:55:38 +0530

    Switched Returning User

--- backend/resources/profile.py
+++ backend/resources/profile.py
@@ -124,7 +124,7 @@ class RemoveFollower(Resource):
         current_user.update(Following=k)
         current_user.FollowingCount-=1
         current_user.save()
-        return make_response(jsonify({'profile':current_user}))
+        return make_response(jsonify({'profile':other_user}))
 
 
 
