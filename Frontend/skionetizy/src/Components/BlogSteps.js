c2ec33a2ee1b79beafd9d4696da8870f2cdc674b
manu-xo5 <mohit.matwaya@gmail.com>
2021-09-06 13:46:30 +0530

    Add navigation on blog steps

--- Frontend/skionetizy/src/Components/BlogSteps.js
+++ Frontend/skionetizy/src/Components/BlogSteps.js
@@ -1,8 +1,20 @@
 import React from "react";
 import styles from "./BlogSteps.module.css";
 import clsx from "../utils/clsx";
+import { useHistory, useLocation } from "react-router-dom";
 
-function BlogSteps({ noOfSteps, currentStep }) {
+function noop() {}
+
+const stepsPathMap = {
+  1: "/addBlogDetailsMarkdown",
+  2: "/addBlogImage",
+  3: "/addBlogKeywords",
+};
+
+function BlogSteps({ noOfSteps, currentStep, onStepClick = noop }) {
+  const history = useHistory();
+  const location = useLocation();
+
   return (
     <div className={styles.wrapper}>
       {Array(noOfSteps)
@@ -22,6 +34,10 @@ function BlogSteps({ noOfSteps, currentStep }) {
                 styles.stepCircle,
                 currentStep === idx + 1 && styles.active
               )}
+              onClick={() => {
+                const path = stepsPathMap[idx + 1];
+                history.push(path, location.state);
+              }}
             >
               {idx + 1}
             </p>
