7a99f75a5d1bbf67f4e01c8b51d76409f5b64a2c
manu-xo5 <mohit.matwaya@gmail.com>
2021-09-25 18:44:58 +0530

    Add new page routes

--- Frontend/skionetizy/src/Components/Button.js
+++ Frontend/skionetizy/src/Components/Button.js
@@ -2,8 +2,10 @@ import React from "react";
 import styles from "./Button.module.css";
 import clsx from "../utils/clsx";
 import Spinner from "./Spinner";
+import { Link } from "react-router-dom";
 
 export default function Button({
+  link,
   isLoading,
   disabled,
   variant = "primary",
@@ -12,6 +14,17 @@ export default function Button({
   children,
   ...props
 }) {
+  if (link === true) {
+    return (
+      <Link
+        className={clsx(styles[variant], styles[size], className, styles.base)}
+        {...props}
+      >
+        {children}
+      </Link>
+    );
+  }
+
   return (
     <button
       className={clsx(styles[variant], styles[size], className, styles.base)}
