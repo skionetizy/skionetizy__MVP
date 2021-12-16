9c9f9a2a4c2b314852f2cf781f92d850d8226c79
Saurabh Kumar Mahra <sourabhmahra91@gmail.com>
2021-12-16 23:28:47 +0530

    deleted sitemap.xml

---
+++ sitemap.py
@@ -0,0 +1,11 @@
+import os
+import datetime
+
+with open("sitemap.xml", "w") as f:
+    """
+    <url>
+        <loc> http://www.papersdrop.com/view-blog/e8d1b624-747a-4842-b35b-44abe1fac2e0/2d7d15cf-8985-4a49-8787-93f2407d338c/</loc>
+        <lastmod> 2021-10-08T18: 30: 00+00: 00 </lastmod >
+    </url>
+        
+    """
