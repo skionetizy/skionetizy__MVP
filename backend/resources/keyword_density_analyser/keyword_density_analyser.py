cb8a5834cb3603ff7a3803b750edb9ccc3b76ac7
jvnganesh <87821979+jvnganesh@users.noreply.github.com>
2022-02-12 12:22:50 +0400

    Create keyword_density_analyser.py

---
+++ backend/resources/keyword_density_analyser/keyword_density_analyser.py
@@ -0,0 +1,107 @@
+import re
+import pickle
+text = '''Best Laptops for students under 65000 INR in India Get the Best laptops for students under 65000 INR in India, I have listed the top 5 best laptops for students with common features like Core i5,Ram 8GB, windows 10, Integrated graphics etc.Laptops can also be used for video editing ,programming , writing, gaming too. These are the best laptops for students in 2021.ASUS VivoBook 2021, AMD Ryzen 5, 15.6-inch , , Light Laptop (8GB | 1TB HDD 256GB SSD ) Laptop Integrated Graphics | 1.8 kg
+
+This Laptop stands at the first place on the list of Best Laptops for students under 65000 INR in India
+
+Bootup is very faster as it has SSD. AMD Ryzen 5500 beats the Intel I5 in the performance and it is very much faster than it. backlit keyboard has a good soft push and makes the typing very comfortable - wakeup time is pretty short, and opens real quick - you will get satisfied with all Win11 compatibility checks – has upgrade option for RAM & SSD, so for sure going for an upgrade – the screen is big and makes you feel satisfied while browsing – camera work swell with teams and zoom meetings.
+price 66990
+processor 2.1GHz Base Speed, : AMD Ryzen 5 Processor
+operatingSystem Loaded with Windows 10 , and lifetime
+preinstalledSoftware Microsoft Office Home & Office 2019, Alexa Built-in
+display 15.6-inch FHD (1920 x 1080) 16:9, 250nits LED Backlit LCD, Anti-glare IPS-level Panel
+memoryAndStorage 8GB DDR4 3200MHz, Support up to 12GB
+graphics Integrated AMD Radeon Graphics
+designAndBattery Up to 8 hours battery life ;Note: Battery life depends on conditions of usage Laptop weight: ,1.8 kg
+Lenovo Legion Laptop (Core i5 8th Gen| 8 GB RAM|1 TB HDD| Windows 10) Laptop
+T5.6-inch FHD Laptop ( 4GB NVIDIA GTX 1050 Graphics | Black | 2.3Kg)
+
+This Laptop stands at the second place on the list of Best Laptops for students under 65000 INR in India
+
+1. Performance – this laptop is a monster. With the Intel core i5 -at its core and NVidia Grpahics is making the gaming experience wonderful ,laptop can sustain 60FPS on most games at high settings.
+
+2. The best part about the laptop is the dual storage. The 128GB SSD really speeds things up. Boot up is bolt and flashing (less than 10 secs). Game load times are much faster
+
+3. , Overwatch, CS & Dota. Everything ran butter smooth I was agreeably shocked by the thermal performance. The fans turned on, but they are not so much loud. There was no throttling whatsoever. I also work on Premier pro and Adobe Photoshop ,it works remarkably well.
+
+The looks -. It resembles a brushed metal surface and bonus Legion logo. Even though it is a gaming laptop, The build quality is excellent and looks premium.
+
+The display The HD display is beautiful and lucid. Overall, I love the display. Display is the best part of Lenovo Legion
+
+Overall
+
+Inspite of the battery problem, I however believe this is one of the best gaming/performance laptops out there in this price range. I adore the thought to small details like having all the ports at the back to guarantee that your desk does not look cluttered, to a 180-degree pivot, which assures you do not accidentally damage your hinge. Harman speakers is as great , as you can expect the best sound quality in a laptop.. If you are someone who is looking for a budget and performing laptop in the market, you should definitely consider checking it out
+price 59,990
+processor Intel I5-processor 8th Generation Core, base speed- 2.3 GHz
+operatingSystem Loaded with windows 10
+preinstalledSoftware Windows 10 Home
+display 15.6-inch screen with (1920X1080) full HD display | Anti Glare technology | 60 FPS
+memoryAndStorage 8 GB RAM with 4GB NVIDIA GTX 1050 Graphics
+graphics Intel Iris Xe Graphics
+designAndBattery Astonishing design for a Gaming laptop , White Backlit keyboard is a great Addon | Battery of upto 8 hours
+warranty comes with 1 year onsite domestic warranty from Lenovo covering manufacturing defects and not covering physical damage.
+HP Pavilion | Microsoft Home |11th Gen Core i5 Laptop | 16 GB RAM, 512GB SSD Laptop
+16 GB RAM, 512GB SSD, Iris Xe Graphics, 14-inch (35.56 cms) FHD Screen, Win 10, MS Office, Backlit Keyboard, Natural Silver (14-dv0054TU)
+
+This Laptop stands at the third place on the list of Best Laptops for students under 65000 INR in India
+
+This is the finest laptop offered by Hp , the laptop serves the purpose for day-to-day programming, business/personal tasks. The processor of this laptop, can easily handle heavy programming ,it can also handle video editing . You can also work on Photoshop too. . The laptop looks very classy. without compromising with the performance. The read-write operations , boot-up speed, are super quick, also makes the laptop boot up very quickThe way the laptop looks is stunning, the Backlit keyword makes it more attractive.
+price 66990
+processor 11th Gen Intel Core i5-1135G7
+operatingSystem It has windows 10 preloaded with lifetime validity.
+preinstalledSoftware Microsoft Office Home & Office 2019, Alexa Built-in
+display 14-inch FHD Anti-Glare Panel
+memoryAndStorage 16GB DDR4-3200 SDRAM | 512 GB SSD
+graphics Intel Iris Xe Graphics
+Dell Inspiron 15-inch Laptop (8GB 1TB HDD + 256 GB SSD ) Laptop
+11th Gen i5 256GB SSD |Win 10 + MS Office |2GB Graphics
+
+This Laptop stands at the fourth place on the list of Best Laptops for students under 65000 INR in India
+
+Dell Inspiron laptop comes on the 4th place.with sleek design and it is best product for students with nice anti glare. 250GB SSD system disk is blazing fast and 1TB HHD is big enough. Works smootly, and screen quality is also good. 2GB NVIDIA makes graphical works buttery smooth, There is no noise & heating issue. MS office student version with lifetime validity is an extra bonus.
+price 65,499
+processor :11th Generation Intel Core i5-1135G7 Processor (
+operatingSystem Windows 10 Home
+preinstalledSoftware Microsoft Office Home & Student 2019
+display 15.6-inch FHD Anti-Glare
+memoryAndStorage 250GB SSD system disk
+graphics 2 GB NVIDIA Graphics
+Acer Aspire , (Intel Core i5 11th ) Laptop
+15.6” inch Laptop| 8GB RAM | 512GB SSD
+
+This Laptop stands at the fifth place on the list of Best Laptops for students under 65000 INR in India
+
+Acer Aspire is a Excellent product at a good pricing point and 11th Generation with 8GB and 512 SSD. Good for student use. User experience is also great. SSD boot times are amazing. the laptop has good specs as per the price , it doesn’t cause any lags. The SSD made my laptop work fast, it also helps the laptop to handle the day to day tasks smootly
+price 66990
+processor Intel Core i5 11th Generation
+operatingSystem Pre-loaded Windows 10 Home with lifetime validity
+memoryAndStorage 8GB RAM upgradable up to 20 GB and 512GB SSD
+warranty One-year International Travelers Warranty (ITW)'''
+
+text = text.lower()
+text = re.sub(r'[\d]+', '', text)
+clean_text = re.sub(r'[,:;/+–\-|().]', ' ', text)
+clean_text = re.sub(r'\s+', ' ', clean_text)
+total_count = len(clean_text.split())
+keywords = [set(clean_text.split()), set(), set()]
+final = [[], [], []]
+
+parts = re.split(r'[,:;/+–\-|().]', text)
+for i in range(2, 4):
+    for part in parts:
+        temp = part.split()
+        length = len(temp)
+        for j in range(0, length - i + 1):
+            phrase = ' '.join(temp[j: j + i])
+            keywords[i - 1].add(phrase)
+
+for i in range(3):
+    for word in keywords[i]:
+        count = len(re.findall(fr'\b{word}\b', clean_text))
+        if len(word) > 2 and count > 1:
+            density = f'{round(count * (i+1) * 100 / total_count, 2)}%'
+            final[i].append((word, count, density))
+    final[i] = sorted(final[i], key=lambda a: a[1], reverse=True)
+
+for idk in final:
+    print(idk)
