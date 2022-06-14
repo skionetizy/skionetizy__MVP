99543ce1b07eb73ea7c78e251db45cc8e586be75
Sanyam-Jain-VIT <sanyam.jain2020@vitstudent.ac.in>
2022-06-14 18:43:02 +0530

    Added the link for canonical, updated the form for careers, and added link for the blogs in profile

--- Frontend/skionetizy/src/Components/UserBlogsCard.js
+++ Frontend/skionetizy/src/Components/UserBlogsCard.js
@@ -7,70 +7,78 @@ import { Link } from "react-router-dom";
 import BlogStatusBadge from "../Components/BlogStatusBadge";
 import styles from "./UserBlogsCard.module.css";
 import { mdToDraftjs } from "draftjs-md-converter";
+import { REDIRECTED_FROM } from "../utils/localStorageKeys";
 
 export default function UserBlogsCard({ blog, profile, isOwner }) {
   const dispatch = useDispatch();
-  const { blogImageURL, blogTitle, blogDescription, blogStatus, timestamp } =
+  const { blogID, blogImageURL, blogTitle, blogDescription, blogStatus, timestamp } =
     blog;
   const { profilePicImageURL, profileName } = profile;
   const blogPublishDate = new Date(timestamp.$date).toLocaleString("en-IN", {
     month: "short",
     day: "numeric",
   });
+
+  const redirecting = () => {
+    localStorage.setItem(REDIRECTED_FROM, `/${profileName}/${blogTitle}/${blogID}`);
+  }
+
   // https://stackoverflow.com/questions/44227270/regex-to-parse-image-link-in-markdown
   var regex=/!\[[^\]]*\]\((.*?)\s*("(?:.*[^"])")?\s*\)/g
   return (
-    <div className={styles.wrapper}>
-      <img
-        src={blogImageURL}
-        className={styles.blogImage}
-        alt={`${blogTitle}'s`}
-      />
+    <Link className={styles.blogLink} onclick={redirecting} to={`/${profileName}/${blogTitle}/${blogID}`}>
+      <div className={styles.wrapper}>
+        <img
+          src={blogImageURL}
+          className={styles.blogImage}
+          alt={`${blogTitle}'s`}
+        />
 
-      {/* details */}
-      <div className={styles.detailsWrapper}>
-        <div className={styles.titleWrapper}>
-          <p className={styles.blogTitle}>{blogTitle}</p>
-          <p>{blogPublishDate}</p>
-        </div>
-
-        <div className={styles.profileDetailsWrapper}>
-          <div className={styles.profileWrapper}>
-            <img
-              src={profilePicImageURL}
-              className={styles.profileImage}
-              alt={profileName}
-            />
-
-            <p className={styles.profileName}>{profileName}</p>
+        {/* details */}
+        <div className={styles.detailsWrapper}>
+          <div className={styles.titleWrapper}>
+            <p className={styles.blogTitle}>{blogTitle}</p>
+            <p>{blogPublishDate}</p>
           </div>
 
-          {isOwner ? blogStatus && (
-            <BlogStatusBadge variant={blogStatus} className={styles.blogStatus}>
-              {blogStatus}
-            </BlogStatusBadge>
-          ):<></>}
-        </div>
+          <div className={styles.profileDetailsWrapper}>
+            <div className={styles.profileWrapper}>
+              <img
+                src={profilePicImageURL}
+                className={styles.profileImage}
+                alt={profileName}
+              />
 
-        {/* <p className={styles.blogDescription}>
-          {blogDescription.substr(0, 200)}...
-        </p> */}
-        <ReactMarkdown className={styles.blogDescription} source={blogDescription.substr(0,100).replace(regex, '')} />
+              <p className={styles.profileName}>{profileName}</p>
+            </div>
+
+            {isOwner ? blogStatus && (
+              <BlogStatusBadge variant={blogStatus} className={styles.blogStatus}>
+                {blogStatus}
+              </BlogStatusBadge>
+            ):<></>}
+          </div>
+
+          {/* <p className={styles.blogDescription}>
+            {blogDescription.substr(0, 200)}...
+          </p> */}
+          <ReactMarkdown className={styles.blogDescription} source={blogDescription.substr(0,100).replace(regex, '')} />
+        </div>
+        {isOwner?
+          <Link
+            to="/addBlogDetailsMarkdown"
+            onClick={() => {
+              dispatch({ type: "MARKDOWN_MODE", payload: "update" });
+              localStorage.setItem("CURRENT_EDITING_BLOG", JSON.stringify(blog));
+              let contentState = convertFromRaw(mdToDraftjs(blog.blogDescription));
+              localStorage.setItem("content",JSON.stringify(convertToRaw(contentState)));
+            }}
+            className={styles.editDraftBtn}
+          >
+            <FiEdit2 className={styles.edit_icon} width="1em" />
+          </Link>
+          :<></>}
       </div>
-      {isOwner?
-        <Link
-          to="/addBlogDetailsMarkdown"
-          onClick={() => {
-            dispatch({ type: "MARKDOWN_MODE", payload: "update" });
-            localStorage.setItem("CURRENT_EDITING_BLOG", JSON.stringify(blog));
-            let contentState = convertFromRaw(mdToDraftjs(blog.blogDescription));
-            localStorage.setItem("content",JSON.stringify(convertToRaw(contentState)));
-          }}
-          className={styles.editDraftBtn}
-        >
-          <FiEdit2 className={styles.edit_icon} width="1em" />
-        </Link>
-        :<></>}
-    </div>
+    </Link>
   );
 }
