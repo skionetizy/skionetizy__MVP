629e69a64a29be21d195e59efdec3fba65016152
rajkaran <rajkaranvir456@gmail.com>
2022-07-08 19:53:41 +0530

    Added filter by UI in FrameBorder

--- Frontend/skionetizy/src/Components/FrameBorder.js
+++ Frontend/skionetizy/src/Components/FrameBorder.js
@@ -1,11 +1,26 @@
 import React from "react";
 import styles from "./FrameBorder.module.css";
 import clsx from "../utils/clsx";
+import TuneIcon from '@material-ui/icons/Tune';
 
 function FrameBorder({ title, className, children }) {
   return (
     <>
-      <div className={clsx(styles.divider, className)}>{title}</div>
+      <div className={clsx(styles.divider, className)}>
+        {title}
+        <span className={clsx(styles.filter, className)}>
+          <p>Filter By</p>
+          <div className={clsx(styles.dropdown)}>
+            <TuneIcon className={clsx(styles.dropbtn)} fontSize="large" />
+            <div className={clsx(styles.dropdownContent, className)}>
+              <a href="#">Old</a>
+              <hr />
+              <a href="#">New</a>
+            </div>
+          </div>
+        </span>
+
+      </div>
       {children}
       <div className={clsx(styles.bottomDivider, className)}></div>
     </>
